aeda65f2c51aae0febda99e3b6689aee8c833bbe
b1012011 <k.k19940107k.t20040105@gmail.com>
2015-04-14 18:14:02 +0900

    標準入出力に日本語対応

--- tutorial.py
+++ tutorial.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from github import Github
 import getpass
 gusername = raw_input("Github_ID>")
 gpassword = getpass.getpass()
+
 g = Github(gusername,gpassword)
 
-#export repository
+#export repositories name
 for repo in g.get_user().get_repos():
     print repo.name
-
+    print repo.id
+    print ""
