d9dc73eb267ce5fe940d3b1d2c9f6dfe45341ff3
b1012011 <k.k19940107k.t20040105@gmail.com>
2015-05-22 17:02:49 +0900

    入力した文字列から名詞のみ抽出し、mecabtxt/keywordに出力

--- mecab.py
+++ mecab.py
@@ -3,17 +3,18 @@
 #mecab.py
 
 import MeCab
-
-
+import pickle
 text = raw_input("text>")
 m = MeCab.Tagger("-Ochasen")
-
 node = m.parseToNode(text)
+
+f = open('mecabtxt/keyword','a')
+norm = []
 while node:
     if node.feature.split(",")[0] == "名詞":
         print node.surface
-    node = node.next
-        
-    #f = open('mecabtxt/mtxt','a')
-    #f.write(mtxt)
-    #f.close()
+        f.write(node.surface)
+        f.write("\n")
+        #norm.append(node.surface)
+    node = node.next        
+f.close()
